68aeff8abcf1b8d6694e13b21c8a24aad9a7f466
Piyush Raj <raj4piyush@gmail.com>
2025-07-13 21:18:58 +0530

    Day 3 added assignmentOperator.cpp

---
+++ Day 3 Operators/assignmentOperator.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 10;
+    a += 1; // Shortcut for a = a+1
+    cout << a << endl;
+    a -= 1; // Shortcut for a = a-1
+    cout << a << endl;
+    a *= 3; // Shortcut for a = a*3
+    cout << a << endl;
+    a/= 5; // Shortcut for a = a/5
+    cout << a << endl;
+    return 0;
+}
