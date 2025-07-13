d4998750995204fb488770cf52ed5ccdd1b98cec
Piyush Raj <raj4piyush@gmail.com>
2025-07-13 10:19:45 +0530

    Day 3 added explicitConversion.cpp

---
+++ Day 3 Operators/explicitConversion.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout << (int)('A') << endl;
+    float PI = 3.14;
+    cout << (int)(PI) << endl;
+    cout << ((float)10/3) << endl;
+    cout << (char)('A'+ 1) << endl;
+    return 0;
+}
