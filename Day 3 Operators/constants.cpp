34fcdbbeec611bdfc3b802be33d4210adc291bc2
Piyush Raj <raj4piyush@gmail.com>
2025-07-13 07:22:31 +0530

    Day 3 constants.cpp

---
+++ Day 3 Operators/constants.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+#define ll long long
+int main(){
+    const int n = 10;
+    long long z = 11; // Instead of two long long we can define a macro for that
+    ll p = 11;
+    return 0;
+}
