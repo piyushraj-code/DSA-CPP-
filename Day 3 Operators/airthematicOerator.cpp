7ac164434b55311f3e6e2d17470f3aef1211b23b
Piyush Raj <raj4piyush@gmail.com>
2025-07-13 20:03:52 +0530

    Day 3 added airthematicOperator.cpp

---
+++ Day 3 Operators/airthematicOerator.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a = 10;
+    int b = 4;
+    // Addition operator
+    int sum = a+b;
+    cout << sum << endl;
+    // Substraction operator
+    int sub = a-b;
+    cout << sub << endl;
+    // Multiplication operator
+    int mul = a*b;
+    cout << mul << endl;
+    // Division operator
+    float div = (float)a/b;
+    cout << div << endl;
+    // Modulo operator
+    int remainder = a%b;
+    cout << remainder << endl;
+    return 0;
+}
