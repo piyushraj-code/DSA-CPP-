c2708d3db342f9f928b29699da2e0f474594144b
Piyush Raj <raj4piyush@gmail.com>
2025-07-13 20:42:56 +0530

    Day 3 added urinaryOperator.cpp

--- Day 3 Operators/airthematicOerator.cpp
+++ Day 3 Operators/airthematicOerator.cpp
@@ -1,22 +1,23 @@
 #include <iostream>
 using namespace std;
-int main(){
+int main()
+{
     int a = 10;
     int b = 4;
     // Addition operator
-    int sum = a+b;
+    int sum = a + b;
     cout << sum << endl;
     // Substraction operator
-    int sub = a-b;
+    int sub = a - b;
     cout << sub << endl;
     // Multiplication operator
-    int mul = a*b;
+    int mul = a * b;
     cout << mul << endl;
     // Division operator
-    float div = (float)a/b;
+    float div = (float)a / b;
     cout << div << endl;
     // Modulo operator
-    int remainder = a%b;
+    int remainder = a % b;
     cout << remainder << endl;
     return 0;
 }
---
+++ Day 3 Operators/urinaryOperator.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+    // Increment Operator
+    int a = 10;
+    cout << ++a << endl;
+    a++;
+    cout << a  << endl;
+    cout << a++ << endl;
+    cout << a << endl;
+    // Decrement Operator
+    int b = 10;
+    cout << --b << endl;
+    cout << b << endl;
+    cout << b-- << endl;
+    cout << b << endl;
+    return 0;
+}
