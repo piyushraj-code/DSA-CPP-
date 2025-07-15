1500edafb92c77ab3ee6b171f50dcb6e8611d36a
Piyush Raj <raj4piyush@gmail.com>
2025-07-15 07:54:44 +0530

    Day 4 added forLoops.cpp

---
+++ forLoops.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    for(int i = 0; i < 10; i++){
+        cout << i+1 << endl;
+    }
+
+    // Print Piyush Raj 5 times
+    for (int i = 0; i < 5; i++){
+        cout << "Piyush Raj" << endl;
+    }
+    return 0;
+}
