bced8c959e3e2f2f53fe55eb22537f526e47353b
Piyush Raj <raj4piyush@gmail.com>
2025-07-17 08:12:21 +0530

    Day 5 added rhombusPattern.cpp

---
+++ Day 6 Patterns/rhombusPattern.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout << "Enter n: ";
+    cin >> n;
+    for (int i = 0; i < n; i++){
+        // Spaces
+        for(int j = 0; j < n-i-1; j++){
+            cout << " ";
+        }
+        // Stars
+        for (int j = 0; j < n; j++){
+            cout << "*";
+        }
+        cout << endl;
+    }
+    return 0;
+}
