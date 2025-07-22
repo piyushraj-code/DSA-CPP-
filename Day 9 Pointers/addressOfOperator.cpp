4d2f38550545fa33cef37c1fbcb577d065dfedfb
Piyush Raj <raj4piyush@gmail.com>
2025-07-22 12:56:16 +0530

    day 9 added addressOfOperator.cpp

---
+++ Day 9 Pointers/addressOfOperator.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a = 10;
+    cout << &a << endl; // & is address of operator which returns the memory location of variable
+    return 0;
+}
