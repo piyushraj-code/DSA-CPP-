f7d7a9a25dc2722489388831a14365f794eb486d
Piyush Raj <raj4piyush@gmail.com>
2025-07-23 20:28:22 +0530

    day 10 added arrayInputOutput.cpp

---
+++ Day 10 Arrays Part 1/arrayInputOutput.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main(){
+    // Input
+    int n;
+    cout << "Enter size of array: ";
+    cin >> n;
+    int arr[5];
+    // int n = sizeof(arr)/sizeof(int);
+    for (int i = 0; i < n; i++){
+        cout << "Enter marks of subject " << i+1 << ": ";
+        cin >> arr[i];
+    }
+    // Output
+     for (int i = 0; i < n; i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
